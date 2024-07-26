5e58a979e07b075afb938ecd6a35c6b89a0c042d
selengecagin <selengecagin@gmail.com>
2024-07-26 16:29:55 +0300

    Add constructor to receive firstName and lastName

--- src/emailapp/Email.java
+++ src/emailapp/Email.java
@@ -8,4 +8,20 @@ public class Email {
    private int mailboxCapacity;
    private String alternateEmail;
 
+//Constructor to receive firstName and lastName
+    public Email(String firstName,String lastName){
+        this.firstName=firstName;
+        this.lastName=lastName;
+    }
+
+
+//Ask for the department
+//Generate random password
+//Set the mailbox capacity
+//Set the alternate email
+//Change the password
+
+
+
 }
+
