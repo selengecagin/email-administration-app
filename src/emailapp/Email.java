2d05a61e36d3e1d5b1aa21bb609f964499a21c21
selengecagin <selengecagin@gmail.com>
2024-07-26 16:57:38 +0300

    Add department confirmation output in Email constructor

--- src/emailapp/Email.java
+++ src/emailapp/Email.java
@@ -15,24 +15,25 @@ public class Email {
         this.firstName=firstName;
         this.lastName=lastName;
         System.out.println("Email Created: " + this.firstName+" "+this.lastName);
-        //Call a method asking for the department, return the department
 
+        //Call a method asking for the department, return the department
         this.department = setDepartment();
+        System.out.println("Department: "+this.department);
     }
 
 
 //Ask for the department
     public String setDepartment() {
-        System.out.println("Enter the department\n1 for Sales\n2 for Development\n3 for Accounting\n0 for none ");
+        System.out.println("Department Codes \n1 for Sales\n2 for Development\n3 for Accounting\n0 for none\nEnter Department Code: ");
         Scanner in = new Scanner(System.in);
         int deptChoice = in.nextInt();
 
         if(deptChoice == 1){
-            return "sales";
+            return "Sales";
         } else if (deptChoice == 2) {
-            return "dev";
+            return "Development";
         } else if (deptChoice == 3) {
-            return "acct";
+            return "Accounting";
         }else {
             return "";
         }
